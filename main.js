ac07966c651c838b68390ad25bdb29cfd352af06
Lazlo <lopezslazarus@gmail.com>
2021-07-24 12:55:08 -0700

    Added task.js file to practice design pattern creation

--- main.js
+++ main.js
@@ -1,3 +1,12 @@
-(function(){
-    console.log("Hello! Welcome to the website test!");
-})();
+// var Task = require('./task');
+import Task from './task';
+
+var task1 = new Task('create a demo for constructors');
+var task2 = new Task('create a demo for modules');
+var task3 = new Task('create a demo for singletons');
+var task4 = new Task('create a demo for prototypes');
+
+task1.complete();
+task2.save();
+task3.save();
+task4.save();
---
+++ task.js
@@ -0,0 +1,29 @@
+let Task = function(name) {
+
+    this.name = name;
+    this.completed = false;
+    
+    this.complete = function() {
+        console.log('completing task: ' + Task.name);
+        this.completed = true;
+    }
+    
+    this.save = function() {
+        console.log("saving task: " + this.name);
+    }
+    
+}
+
+// ClassName.prototype.methodName = function(arguments) {
+// };
+
+Task.prototype.complete = function() {
+    console.log('completing task: ' + Task.name);
+    this.completed = true;
+};
+
+Task.prototype.save = function() {
+    console.log("saving task: " + this.name);
+};
+
+module.exports = Task;
