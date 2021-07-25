868077a9856724dd4997c567968106b117d26755
Lazlo <lopezslazarus@gmail.com>
2021-07-25 10:07:10 -0700

    Added More Functionality with modulePractice.js

--- main.js
+++ main.js
@@ -1,12 +1,4 @@
-// var Task = require('./task');
-import Task from './task';
+// import Task from './task';
 
-var task1 = new Task('create a demo for constructors');
-var task2 = new Task('create a demo for modules');
-var task3 = new Task('create a demo for singletons');
-var task4 = new Task('create a demo for prototypes');
-
-task1.complete();
-task2.save();
-task3.save();
-task4.save();
+var Task = require('./task');
+var Temp = require('./modulePractice');
